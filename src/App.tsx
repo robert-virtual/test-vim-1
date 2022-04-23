03cb23fed9eca4c5d0f438381c4fafceb10ab087
robert-virtual <robertocastill63@gmail.com>
2022-04-23 15:05:16 -0600

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,49 @@
+import { useState } from "react";
+interface INote {
+  done: boolean;
+  text: string;
+}
 function App() {
+  const [notes, setNotes] = useState<INote[]>([]);
+  const [note, setNote] = useState("");
+  function saveNote() {
+    setNotes([...notes, { done: false, text: note }]);
+    setNote("");
+  }
+  function updateNote(idx: number) {
+    return () => {
+      setNotes((prev) =>
+        prev.map((n, i) => (i == idx ? { ...n, done: !n.done } : n))
+      );
+    };
+  }
   return (
     <div>
       <h2>Notes App - With Vim</h2>
+      <input
+        type="text"
+        placeholder="encriba una nota..."
+        value={note}
+        onChange={({ target }) => setNote(target.value)}
+        onKeyDown={({ key }) => {
+          if (key == "Enter") {
+            saveNote();
+          }
+        }}
+      />
+      {notes.map(({ done, text }, i) => (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+          key={i.toString()}
+        >
+          <input type="checkbox" checked={done} onChange={updateNote(i)} />
+          <span>{text}</span>
+        </div>
+      ))}
     </div>
   );
 }
